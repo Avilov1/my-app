ce619e1ddea90bb20c42795b140624c0cb35f0b4
Avilov1 <dm.avilov@it-justice.com>
2021-10-27 15:57:44 +0300

    created develop branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Init my react app and created develop branch
         </a>
       </header>
     </div>
